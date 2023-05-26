610b0c46bfffb5371d2c2d414a70e329a62f5415
Happer World <happer64bit@gmail.com>
2023-05-26 18:09:17 +0630

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Rubik } from 'next/font/google'
 import { Providers } from './provider'
+import { Analytics } from '@vercel/analytics/react';
 
 const rubik = Rubik({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <meta name="google-site-verification" content="0hbZKdBjVysNUDD6kor0Hd6lN-aI78I8v5NhYIXa30o" />
       </head>
       <body className={rubik.className}>
+        <Analytics />
         <Providers>
           {children}
         </Providers>

98650fa065a7c0669f41c34dbc956f2594ac64ab
Happer World <happer64bit@gmail.com>
2023-06-11 15:44:43 +0630

    it sucks

--- app/api/findPost/route.ts
+++ app/api/findPost/route.ts
@@ -15,7 +15,7 @@ export async function GET(request: Request) {
     
     try {
         // Use prisma.posts to fetch posts based on the limit and page parameters
-        const posts = await prisma.posts.findMany({
+        const posts = await prisma.post.findMany({
             where: {
                 id: postID
             }

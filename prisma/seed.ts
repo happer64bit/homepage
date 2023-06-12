48ecdd67cb773a62d52e394161c0ed74549212ac
Happer World <happer64bit@gmail.com>
2023-06-12 20:10:09 +0630

    Push Blah Blah Blah

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,66 +1,29 @@
 const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
-   const post = await prisma.post.create({
+  try {
+    const filePath = ".md.md"; // Replace with the actual file path
+
+    // Read the file contents
+    const contents = fs.readFileSync(filePath, "utf-8");
+
+    const post = await prisma.post.create({
       data: {
-         title: "Demystifying Nginx: A Powerful Web Server and Reverse Proxy".toUpperCase(),
-         contents: `
-# Demystifying Nginx: A Powerful Web Server and Reverse Proxy
-
-Nginx (pronounced "engine-x") is a high-performance web server and reverse proxy server that has gained significant popularity in recent years. It is known for its speed, scalability, and versatility, making it a popular choice for serving web content, load balancing, and handling various networking tasks. In this article, we'll explore the key features and use cases of Nginx.
-            
-## What is Nginx?
-            
-Nginx is an open-source web server software that was initially developed to solve the C10k problem, which refers to handling a large number of concurrent connections efficiently. It was designed to have a small memory footprint, low CPU usage, and high scalability, making it an excellent choice for serving static and dynamic content.
-            
-Unlike traditional web servers like Apache, Nginx follows an event-driven, asynchronous architecture, which allows it to handle a large number of simultaneous connections with minimal system resources. It is commonly used to serve static files, host websites, and act as a reverse proxy server.
-            
-## Key Features of Nginx
-            
-### 1. High Performance and Scalability
-
-Nginx is renowned for its exceptional performance and scalability. Its event-driven architecture allows it to handle thousands of concurrent connections while consuming minimal system resources. Nginx is often used in high-traffic websites and applications to ensure fast response times and handle heavy loads efficiently.
-            
-### 2. Reverse Proxy Server
-            
-Nginx is widely used as a reverse proxy server, which sits between client requests and backend servers. It can distribute incoming requests among multiple servers, balance the load, and provide fault tolerance. This enables efficient load balancing, improved security, and seamless handling of backend server failures.
-            
-### 3. Load Balancing
-            
-With Nginx's load balancing capabilities, you can distribute incoming traffic across multiple servers to optimize resource utilization and ensure high availability. Nginx supports various load balancing algorithms, such as round-robin, least connections, IP hash, and more. This makes it suitable for horizontally scaling web applications and handling large amounts of traffic.
-            
-### 4. SSL/TLS Termination
-            
-Nginx can act as an SSL/TLS termination point, handling secure connections and offloading the SSL/TLS encryption and decryption process from backend servers. This improves performance and reduces the computational load on application servers.
-            
-### 5. Caching
-            
-Nginx includes powerful caching mechanisms that allow you to cache static content and API responses. By caching frequently accessed content, Nginx reduces the load on backend servers, improves response times, and enhances overall system performance.
-            
-## Use Cases for Nginx
-            
-Nginx is a versatile web server that can be used in various scenarios, including:
-            
-- Serving static files: Nginx can efficiently serve static HTML, CSS, JavaScript, and media files, offloading this task from application servers.
-            
-- Load balancing: Nginx's load balancing capabilities make it suitable for distributing traffic across multiple servers and ensuring high availability.
-            
-- Reverse proxy server: Nginx acts as a reverse proxy, providing an additional layer of security and load balancing for backend servers.
-            
-- SSL/TLS termination: Nginx can handle SSL/TLS encryption and decryption, freeing up backend servers from this computationally intensive task.
-            
-- Caching: Nginx's caching features can improve performance by serving cached content and reducing the load on backend servers.
-            
-## Getting Started with Nginx
-            
-To get started with Nginx, you'll need to install it on your server or use a pre-configured distribution. Once installed, you can modify the Nginx configuration file to define server blocks, specify locations, set up SSL/T
-`,
-         thumbnail: "https://i.pinimg.com/736x/11/58/48/115848de04aec285cfe32b2ef6363965.jpg"
+        title: "Introducing Myself".toUpperCase(),
+        contents: contents,
+        thumbnail: "https://github.com/happer64bit/happer64bit/blob/main/blog-assets/images/who-is-wintkhantlin-1.png?raw=true"
       }
-   }
-   )
+    });
+
+    console.log("Post created:", post);
+  } catch (error) {
+    console.error("Error creating post:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-main()
+main();

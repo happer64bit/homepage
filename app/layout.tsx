550483d4feca862028438eebd295643d9f2667d4
Happer World <happer64bit@gmail.com>
2023-06-12 21:50:48 +0630

    Added GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Rubik } from 'next/font/google'
 import { Providers } from './provider'
 import { Analytics } from '@vercel/analytics/react';
 import { ColorModeScript } from '@chakra-ui/react';
+import Script from 'next/script';
 
 const rubik = Rubik({ subsets: ['latin'] })
 
@@ -20,6 +21,16 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="0hbZKdBjVysNUDD6kor0Hd6lN-aI78I8v5NhYIXa30o" />
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-T4DJK2QJYT"></Script>
+        <Script>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-T4DJK2QJYT');
+          `}
+        </Script>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico"/>
         <meta name="theme-color" content='#141a20'/>

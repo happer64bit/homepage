e8989e23d780cf1016dca804973618628dc598cb
happer64bit <happer64bit@gmail.com>
2023-10-08 20:37:49 +0630

    Added generateMetadata for articles

--- app/article/[slug]/layout.tsx
+++ app/article/[slug]/layout.tsx
@@ -1,5 +1,36 @@
+import prisma from "@/lib/prisma";
 import React from "react";
 
+export async function generateMetadata({ params } : any) {
+    const article = await prisma.articles.findUnique({
+        where: {
+            pathname: params.slug
+        },
+        select: {
+            contents: false,
+            createdAt: false,
+            id: false,
+            pathname: false,
+            shortContents: true,
+            thumbnail: true,
+            title: true,
+            updatedAt: false
+        }
+    })
+
+    return {
+        title: article?.title,
+        description: article?.shortContents,
+        twitter: {
+            images: article?.thumbnail
+        },
+        openGraph: {
+            albums: article?.thumbnail,
+            images: article?.thumbnail,
+        },
+        "og:image": article?.thumbnail,
+    }
+}
 export default function ArticleLayout({ children } : { children: React.ReactNode }) {
     return (
         <div className="my-20">
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -39,13 +39,34 @@ Join me as I navigate the ever-changing landscape of coding, turning my dreams i
         }
 
     })
-    const event = await prisma.events.create({
+    // const event = await prisma.events.create({
+    //     data: {
+    //         title: "Announcement 📢",
+    //         description: "This is test a announcement",
+    //         redirectLink: "#",
+    //         endDate: "2023-10-09T15:22:37.266Z",
+    //         enabled: true
+    //     }
+    // })
+    const post = await prisma.articles.create({
         data: {
-            title: "Announcement 📢",
-            description: "This is test a announcement",
-            redirectLink: "#",
-            endDate: "2023-10-09T15:22:37.266Z",
-            enabled: true
+            title: "Happy 2 Months Anniversary",
+            contents: `
+<div style="text-align: right">To My Honey <b>Thae Su Moe</b></div>
+
+Having you as my partner is one of the luckiest things that has ever happened to me in my entire life. Since you entered my life, everything has started to change, and I cherish every moment we spend together. Talking to you always brings a smile to my face.
+
+I love you more than ever. You are my first love, and I am yearning to have you as my future wife. I don't care about what people say, blame, or disappoint about us. I don't care about money or whatever you looks like. I just love you.
+
+I love you for your kindness, your laughter that brightens my darkest days, and the way you make me feel like the luckiest person in the world. I can't wait to build a life together, to create memories, and to face the future hand in hand.
+
+No matter the challenges that lie ahead, I know that our love will always be stronger. You mean the world to me, and I cherish every moment we spend together. I look forward to a lifetime of love and happiness with you.
+
+<div style="text-align: right">From <b>Wint Khant Lin</b></div>
+`,
+            pathname: "happy-2-month-anniversary",
+            thumbnail: "https://github.com/happer64bit/happer64bit/blob/main/blog-assets/images/happy-2-month-anniversary.png?raw=true",
+            shortContents: "To my Love, Thae Su Moe😍"
         }
     })
 }

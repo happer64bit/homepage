38cd773bb889c888a2a995e6fe7a6d2793bc0f74
happer64bit <happer64bit@gmail.com>
2024-03-22 08:06:50 +0630

    (fix): Annoucement Bar is not working

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 async function getEvent(): Promise<any> {
     "use server"
-    const currentDate = new Date(); // Current date
+    const currentDate = new Date();
 
     const event = await prisma.events.findFirst({
         orderBy: {
@@ -14,28 +14,32 @@ async function getEvent(): Promise<any> {
     await prisma.$disconnect()
     
     const isEventActive = event && event.startedDate <= currentDate && currentDate <= event.endDate;
-
+    
+    console.log(isEventActive)
+    
     const responseObject = {
-        event,
+        ...event,
         isEventActive
     };
-
+    
+    console.log(responseObject)
+    
     return responseObject
 }
 
 export default async function EventAnnouncement() {
-    const event: any = getEvent()
+    const event: any = await getEvent()
 
     return event && event.isEventActive && (
-        <Link href={event.event.redirectLink}>
+        <Link href={event.redirectLink}>
             <div className="relative isolate flex items-center gap-x-6 overflow-hidden dark:text-white dark:bg-transparent px-6 py-2 sm:px-3.5 justify-center hover:underline">
                 <div className="text-center">
                     <p className="text-sm leading-6">
-                        <strong className="font-semibold">{event.event.title}</strong>
+                        <strong className="font-semibold">{event.title}</strong>
                         <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
                             <circle cx={1} cy={1} r={1} />
                         </svg>
-                        {event.event.description}
+                        {event.description}
                     </p>
                 </div>
             </div>

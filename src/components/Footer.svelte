d77595c15d63ad8ff33600318754bdf5b5d9f2ac
happer64bit <happer64bit@gmail.com>
2024-11-17 12:18:34 +0630

    fixed footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="bg-[#0e0e0e] p-6 container mx-auto rounded-t-xl text-center mt-12">
+<footer class="bg-[#02020] p-6 container mx-auto rounded-t-xl text-center mt-12">
     <p class="font-bold">Wint Khant Lin &copy;</p>
     <p class="opacity-80">Subscribe to my <a href="/rss.xml" class="font-bold underline">RSS Feed</a> for updates</p>
 </footer>

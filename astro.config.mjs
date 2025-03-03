9e1522471799c0552f0b3edb75b943fbf896e410
unknown <happer64bit@gmail.com>
2025-03-04 00:37:37 +0630

    inital

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import swup, { Theme } from '@swup/astro'
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      plugins: [
+          tailwindcss()
+      ]
+  },
+
+  integrations: [icon(), swup({
+    theme: Theme.slide,
+    animationClass: "transition-",
+    cache: true,
+    smoothScrolling: true
+  })]
+});

a12e1b34cb907103b13acedc335fcaba2db9e249
unknown <happer64bit@gmail.com>
2025-03-05 15:31:01 +0630

    added ci

---
+++ unlighthouse.config.js
@@ -0,0 +1,11 @@
+export default {
+  site: 'https://example.com',
+  ci: {
+    budget: {
+      performance: 60,
+      accessibility: 100,
+      'best-practices': 90,
+      seo: 90,
+    }
+  }
+}

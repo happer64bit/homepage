058f92375f327242e5f485446dcdf525a5a4051b
unknown <happer64bit@gmail.com>
2025-03-21 14:25:46 +0630

    add partytown

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { partytownVite } from "@qwik.dev/partytown/utils";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit(), partytownVite({})]
 });

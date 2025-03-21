29edaaf55b1a5c45cf6aef813c5ba60a7ffb554b
unknown <happer64bit@gmail.com>
2025-03-21 14:27:33 +0630

    add api

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<div>
+	<div class="max-w-[1200px] mx-auto my-10 grid gap-5 space-y-4 px-4 lg:grid-cols-3">
+		{#each data.posts as post}
+			<div class="transition-all hover:scale-105">
+				<a href={`/posts/${post.slug}`}>
+					<article class="flex min-h-60 flex-col justify-between space-y-2 bg-[#2f2f2f] p-4 rounded-2xl border-4 border-transparent hover:border-teal-500">
+						<div class="space-y-2">
+							<h2 class="text-2xl font-bold">{post.title}</h2>
+							<div class="flex items-center gap-2">
+								<div class="rounded-lg bg-white/5 px-4 py-0.5 font-bold">{post.tag}</div>
+								<time>{new Date(post.publishedDate).toLocaleDateString()}</time>
+							</div>
+						</div>
+						<p class="line-clamp-3 text-gray-300">{post.summary}</p>
+					</article>
+				</a>
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+</svelte:head>
+
+<div>
+	<div class="prose prose-lg prose-invert container my-20">
+		<data.content />
+	</div>
+</div>

0e6b232fc6f4e77f1785b56ef1be9edcd12d88d5
unknown <happer64bit@gmail.com>
2025-03-21 14:26:27 +0630

    add markdown highligher

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,36 @@
-import { mdsvex } from 'mdsvex';
+import { escapeSvelte, mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { createHighlighter } from 'shiki';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md'],
+			highlight: {
+				highlighter: async (code, lang = 'text') => {
+					const highlighter = await createHighlighter({
+						themes: ['poimandres'],
+						langs: ['javascript', 'typescript', 'css', 'html', 'go']
+					});
+					await highlighter.loadLanguage('javascript', 'typescript', 'css', 'go', 'html');
+					const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }));
+					return `{@html \`${html}\` }`;
+				}
+			},
+			rehypePlugins: []
+		})
+	],
 
 	kit: {
 		adapter: adapter()
 	},
 
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;

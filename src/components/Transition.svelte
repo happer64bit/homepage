c2c2b25c6878a83fcfb3f46baa5c24716a525171
unknown <happer64bit@gmail.com>
2025-03-21 18:57:14 +0630

    add transition

---
+++ src/components/Transition.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { fly } from 'svelte/transition';
+
+	type Props = {
+		children: Snippet;
+		url: string;
+	};
+
+	let { children, url }: Props = $props();
+</script>
+
+{#key url}
+	<div in:fly={{ y: 200, duration: 300, delay: 300 }} out:fly={{ y: 200, duration: 300 }}>
+		{@render children?.()}
+	</div>
+{/key}

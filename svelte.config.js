f35bb31e40eebfc2b84f4a58501fd8df208e4367
unknown <happer64bit@gmail.com>
2025-03-21 14:34:36 +0630

    changed to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { escapeSvelte, mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { createHighlighter } from 'shiki';
 

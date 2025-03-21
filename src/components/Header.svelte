0245dac08c816bd2e1c7d56139fe893890f11b98
unknown <happer64bit@gmail.com>
2025-03-21 14:26:35 +0630

    add header

---
+++ src/components/Header.svelte
@@ -0,0 +1,7 @@
+<header class="pt-2 px-2 sticky top-0 left-0 z-50">
+    <nav class="container px-6 py-3 bg-white/5 backdrop-blur-lg rounded-lg flex justify-between items-center">
+        <a href="/" class="text-xl font-bold">Wint</a>
+
+        <a href="/posts" class="font-semibold text-gray-300 hover:text-white transition-all">Posts</a>
+    </nav>
+</header>

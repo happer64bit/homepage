5047d6bf1831502723b854123682978371b983da
unknown <happer64bit@gmail.com>
2025-03-21 18:57:03 +0630

    add icon

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,9 @@
 <header class="pt-2 px-2 sticky top-0 left-0 z-50">
-    <nav class="container px-6 py-3 bg-white/5 backdrop-blur-lg rounded-lg flex justify-between items-center">
-        <a href="/" class="text-xl font-bold">Wint</a>
+    <nav class="container px-6 py-3 bg-[#222222cc] backdrop-blur-lg rounded-lg flex justify-between items-center">
+        <div class="flex items-center gap-2">
+            <enhanced:img src="./../../static/favicon-96x96.png" class="h-9 w-9"/>
+            <a href="/" class="text-xl font-bold">Wint</a>
+        </div>
 
         <a href="/posts" class="font-semibold text-gray-300 hover:text-white transition-all">Posts</a>
     </nav>

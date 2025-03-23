7bbe4a6645cfc99fdde57c95bdc3549248eb9b78
unknown <happer64bit@gmail.com>
2025-03-23 09:09:46 +0630

    fix responsiveness on footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,10 @@
 <footer class="py-10 bg-white text-black">
-    <div class="max-w-[1200px] mx-auto px-4 grid grid-cols-2">
+    <div class="max-w-[1200px] mx-auto px-4 grid lg:grid-cols-2">
         <div class="space-y-2">
             <h2 class="font-bold text-4xl">Touch Me In</h2>
             <a href="mailto:wintkhantlin@gmail.com" class="text-xl hover:underline">wintkhantlin@gmail.com</a>
         </div>
-        <div class="flex flex-col justify-end items-end">
+        <div class="flex flex-col justify-end lg:items-end lg:mt-0 mt-10">
             <div class="flex flex-col">
                 <h2 class="text-3xl font-bold mb-2">Connect</h2>
                 <a class="text-xl font-bold transition-all hover:text-[#777]" href="https://github.com/happer64bit" target="_blank">GitHub</a>

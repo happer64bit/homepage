a6cd6b17f581f3bacde6e43a839c3b444803a1a0
unknown <happer64bit@gmail.com>
2025-05-14 16:23:55 +0630

    fix placement

--- src/components/HomeAboutMyRelationship.tsx
+++
@@ -1,9 +0,0 @@
-// import Image from "next/image";
-// import MeAndMyGFImg from '@/assets/meandmygf.jpg'
-
-export default function HomeAboutMyRelationship() {
-    return (
-        <section>
-        </section>
-    )
-}
--- src/components/HomeBlogDisplay.tsx
+++ src/components/HomeBlogDisplay.tsx
@@ -15,7 +15,7 @@ export default function HomeBlogDisplay(props: HomeBlogDisplayProps) {
                     <Link href={`/posts/${post._meta.path}`} key={post._meta.path} className="p-6 rounded-3xl flex flex-col justify-between min-h-[170px] bg-[#E8E8E8]">
                         <div>
                             <h3 className="text-xl">{post.title}</h3>
-                            <p className="uppercase font-sans">{post.publishedDate.toLocaleString()}</p>
+                            <p className="uppercase font-sans mt-1">{post.publishedDate.toLocaleString()}</p>
                         </div>
                         <p className="text-blue-500 flex items-center gap-2">READ ARTICLE <ArrowUpRightIcon /></p>
                     </Link>

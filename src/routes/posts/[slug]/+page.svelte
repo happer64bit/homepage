903b119c581da89af5eb955d76237c811b03fc01
unknown <happer64bit@gmail.com>
2025-05-22 08:00:23 +0630

    better seo

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -5,6 +5,15 @@
 	let { data }: { data: { post: Post } } = $props();
 </script>
 
+<svelte:head>
+	<title>{data.post.title}</title>
+	<meta name="description" content={data.post.summary} />
+	<meta name="keywords" content={data.post.keywords.join(",")} />
+	<meta name="author" content="Wint Khant Lin" />
+	<meta property="og:title" content={`${data.post.title} | Wint Khant Lin`} />
+	<meta property="og:description" content={data.post.summary} />
+</svelte:head>
+
 <article
 	class="prose prose-lg prose-pre:bg-[#fff] prose-pre:border mx-auto max-w-[800px] my-20 px-4"
 >

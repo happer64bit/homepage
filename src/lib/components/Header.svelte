66ce4ca215255a45fefa621ce706d4a8310a428b
unknown <happer64bit@gmail.com>
2025-05-24 10:40:21 +0630

    add cv

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,7 @@
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
 <header class="py-3 sticky top-0 left-0 backdrop-blur-lg z-50">
 	<nav class="grid grid-cols-3 container">
 		<div class="flex items-center gap-4">
@@ -5,8 +9,17 @@
 		</div>
 		<div class="flex items-center justify-center">
 			<a href="/">
-				<img src="/images/WINT.svg" alt="Wint Khant Lin Portfolio Icon" class="w-32"/>
-            </a>
+				<img src="/images/WINT.svg" alt="Wint Khant Lin Portfolio Icon" class="w-32" />
+			</a>
+		</div>
+		<div class="flex justify-end">
+			<a
+				href="/cv.pdf"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="bg-black text-white px-4 py-2 rounded-full flex items-center gap-2 cursor-pointer hover:opacity-80 group"
+				>Resume
+			</a>
 		</div>
 	</nav>
 </header>

a00ada0f00e3d4df161c8c6e3194a886db4f01c6
unknown <happer64bit@gmail.com>
2025-08-08 17:59:35 +0630

    add giscus

--- app/pages/posts/[slug].vue
+++ app/pages/posts/[slug].vue
@@ -1,11 +1,14 @@
 <script setup>
+import { onMounted } from 'vue'
+import { useRoute, useAsyncData, createError, useSeoMeta } from '#imports'
+
 const { params } = useRoute()
 
-const { data: { value: post }, error } = await useAsyncData(`/posts/${params.slug}`, () =>
-  queryCollection("posts").path("/posts/" + params.slug).first()
+const { data: post, error } = await useAsyncData(`post-${params.slug}`, () =>
+  queryCollection('posts').path('/posts/' + params.slug).first()
 )
 
-if (!post) {
+if (!post.value) {
   throw createError({
     statusCode: 404,
     statusMessage: 'Page Not Found',
@@ -13,22 +16,56 @@ if (!post) {
 }
 
 useSeoMeta({
-    title: post.title,
-    description: post.summary,
-    author: "Wint Khant Lin",
-    articleTag: post.categories
+  title: post.value.title,
+  description: post.value.summary,
+  author: 'Wint Khant Lin',
+  articleTag: post.value.categories,
 })
 
+onMounted(() => {
+  const script = document.createElement('script')
+  script.src = 'https://giscus.app/client.js'
+  script.async = true
+  script.crossOrigin = 'anonymous'
+  script.setAttribute('data-repo', 'happer64bit/homepage')
+  script.setAttribute('data-repo-id', 'R_kgDOI1qB4w')
+  script.setAttribute('data-category', 'Blog Posts')
+  script.setAttribute('data-category-id', 'DIC_kwDOI1qB484Ce_ri')
+  script.setAttribute('data-mapping', 'pathname')
+  script.setAttribute('data-strict', '0')
+  script.setAttribute('data-reactions-enabled', '1')
+  script.setAttribute('data-emit-metadata', '0')
+  script.setAttribute('data-input-position', 'bottom')
+  script.setAttribute('data-theme', 'light')
+  script.setAttribute('data-lang', 'en')
+  script.setAttribute('data-loading', 'lazy')
+
+  document.getElementById('giscus-container').appendChild(script)
+})
 </script>
 
 <template>
-    <section class="px-6 max-w-[800px] mx-auto mb-20">
-        <div class="min-h-[30vh] flex items-center">
-            <div>
-                <h1 class="text-4xl">{{ post.title }}</h1>
-                <p class="mt-2 text-neutral-600">Posted At {{ new Date(post.publishedDate).toLocaleDateString().replaceAll("/", "-") }}</p>
-            </div>
-        </div>
-        <ContentRenderer :value="post.body" class="prose prose-lg max-w-full prose-code:bg-transparent prose-pre:border prose-pre:bg-white" />
-    </section>
+  <main class="px-6 max-w-[800px] mx-auto mb-20">
+    <div>
+      <div class="mt-10">
+        <h1 class="text-4xl mb-2">{{ post.title }}</h1>
+        <p class="mb-10 text-neutral-600">
+          Posted At
+          {{
+            new Date(post.publishedDate)
+              .toLocaleDateString()
+              .replaceAll('/', '-')
+          }}
+        </p>
+      </div>
+    </div>
+    <article>
+      <ContentRenderer
+        :value="post.body"
+        class="prose prose-lg max-w-full prose-code:bg-transparent prose-pre:border prose-pre:bg-white"
+      />
+    </article>
+
+    <div id="giscus-container" class="mt-10" />
+  </main>
 </template>

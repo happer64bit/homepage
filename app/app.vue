7594b4ee073dd4ddaa3e2f72c9dd8088f5ee62dc
happer64bit <happer64bit@gmail.com>
2026-01-03 17:52:07 +0630

    fix gtag

--- app/app.vue
+++ app/app.vue
@@ -1,19 +1,18 @@
 <script setup>
-const { proxy } = useScriptGoogleAnalytics({
-  scriptOptions: {
-    trigger: "client"
-  }
+const { proxy } = useScriptGoogleAnalytics({ scriptOptions: { trigger: "client" } })
+const router = useRouter()
+onMounted(() => {
+  proxy.gtag("config", "G-P4LM3B1RV1", { send_page_view: false })
+  router.afterEach((to) => {
+    proxy.gtag("event", "page_view", { page_path: to.fullPath })
+  })
 })
-
-proxy.gtag("config", "default", {
-  send_page_view: true
-})
-
 </script>
 
 <template>
   <NuxtLoadingIndicator color="white" />
-  <div class="max-w-240 p-6 mx-auto">
+  <NavBar />
+  <div class="max-w-300 p-6 mx-auto">
     <NuxtPage />
   </div>
 </template>

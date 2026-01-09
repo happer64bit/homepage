afd47f3604efc7e28112cdfe22621bc64c161dd1
happer64bit <happer64bit@gmail.com>
2026-01-09 07:43:19 +0630

    install @astro/partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
@@ -21,5 +23,5 @@ export default defineConfig({
     ]
   },
 
-  integrations: [icon()]
+  integrations: [icon(), partytown()]
 });
